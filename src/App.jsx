60f2dca102221e7dd0cc0510fd2ea6be9a0115ac
adeyemi <aladesuyiadeyemi05@gmail.com>
2025-05-30 21:46:11 +0100

    Add Netlify and Render configuration files; update routing in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom"
 import './App.css'
 import NaviIQLanding from './Pages/LandingPage'
 import LoginPage from "./Pages/LoginPage"
